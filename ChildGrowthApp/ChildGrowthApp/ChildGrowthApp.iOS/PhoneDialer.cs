4049b5b1a000c2d081868e9afea15cb2132d44f9
kevinjyee <kevinyee@utexas.edu>
2017-09-19 19:47:06 -0500

    first commit

---
+++ ChildGrowthApp/ChildGrowthApp/ChildGrowthApp.iOS/PhoneDialer.cs
@@ -0,0 +1,18 @@
+﻿using ChildGrowthApp;
+using Foundation;
+using ChildGrowthApp.iOS;
+using UIKit;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(PhoneDialer))]
+namespace ChildGrowthApp.iOS
+{
+    public class PhoneDialer : IDialer
+    {
+        public bool Dial(string number)
+        {
+            return UIApplication.SharedApplication.OpenUrl(
+                new NSUrl("tel:" + number));
+        }
+    }
+}
---
+++ ChildGrowthApp/ChildGrowthApp/ChildGrowthApp/IDialer.cs
@@ -0,0 +1,7 @@
+﻿namespace ChildGrowthApp
+{
+    public interface IDialer
+    {
+        bool Dial(string number);
+    }
+}
