cdb6ad07cb133de39ff60c4ee80e2cdc24f8625f
kevinjyee <kevinyee@utexas.edu>
2017-09-24 16:31:47 -0500

    modified scraper

--- ChildGrowth/ChildGrowth/ChildGrowth/scraper.py
+++ ChildGrowth/ChildGrowth/ChildGrowth/scraper.py
@@ -1,8 +1,29 @@
 import pandas as pd
 
+
+
 def readInCSV():
+
+    #const
+    sexChoice = 2
+    type = "Weight"
+
     df = pd.read_csv("data/wtageinf.csv");
-    print df.loc[df['Sex'] == 2, 'Agemos'].tolist();
+    percentileCols = ["P3",	"P5","P10",	"P25",	"P50",	"P75",	"P90",	"P95", "P97"]
+
+    for item in percentileCols:
+        dataList = df.loc[df['Sex'] == sexChoice, item].tolist();
+        print formatList(sexChoice, dataList, item, type)
+
+def formatList(sexChoice, list, percentileCols, type):
+    genderString = "Male"
+    if sexChoice == 2:
+        genderString = "Female";
+
+    Category = percentileCols + genderString + type
+    listString = ','.join(str(e) for e in list);
+    template = "public static List < double >" + Category +  "= new List < double >(new double[] {" + listString + "});"
+    return template
 
 if __name__ == "__main__":
   readInCSV()
