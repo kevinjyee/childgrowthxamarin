5d19c93cbfb6894573945a74b6c8742538dbe02b
jdbravol <jdbravol@utexas.edu>
2017-10-09 12:42:13 -0500

    added description page

---
+++ ChildGrowth/ChildGrowth/ChildGrowth/Pages/Milestones/MilestonesDescriptionPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace ChildGrowth.Pages.Milestones
+{
+    public partial class MilestonesDescriptionPage : ContentPage
+    {
+        public MilestonesDescriptionPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
