b54ab3fd25dac5d45bd10a4cc429348432af9c84
kevinjyee <kevinyee@utexas.edu>
2017-10-14 10:09:45 -0500

    committing card stack view

--- ChildGrowth/ChildGrowth/ChildGrowth/Pages/Milestones/Milestones.xaml.cs
+++ ChildGrowth/ChildGrowth/ChildGrowth/Pages/Milestones/Milestones.xaml.cs
@@ -14,10 +14,53 @@ namespace ChildGrowth.Pages.Milestones
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Milestones : ContentPage
     {
+        CardStackView cardStack;
+        MainPageViewModel viewModel = new MainPageViewModel();
+
         public Milestones()
         {
             InitializeComponent();
+
+
+                this.BindingContext = viewModel;
+                this.BackgroundColor = Color.Black;
+
+                RelativeLayout view = new RelativeLayout();
+
+                cardStack = new CardStackView();
+                cardStack.SetBinding(CardStackView.ItemsSourceProperty, "ItemsList");
+                cardStack.SwipedLeft += SwipedLeft;
+                cardStack.SwipedRight += SwipedRight;
+
+                view.Children.Add(cardStack,
+                    Constraint.Constant(30),
+                    Constraint.Constant(60),
+                    Constraint.RelativeToParent((parent) => {
+                        return parent.Width - 60;
+                    }),
+                    Constraint.RelativeToParent((parent) => {
+                        return parent.Height - 140;
+                    })
+                );
+
+                this.LayoutChanged += (object sender, EventArgs e) =>
+                {
+                    cardStack.CardMoveDistance = (int)(this.Width * 0.40f);
+                };
+
+                this.Content = view;
+            }
+
+            void SwipedLeft(int index)
+            {
+                // card swiped to the left
+            }
+
+            void SwipedRight(int index)
+            {
+                // card swiped to the right
+            }
+           
             
         }
     }
-}   
