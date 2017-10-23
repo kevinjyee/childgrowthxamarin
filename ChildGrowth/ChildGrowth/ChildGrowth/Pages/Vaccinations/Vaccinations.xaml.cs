0e1894fabe3d4bbcc531c4d7605ce0623429e77d
kevinjyee <kevinyee@utexas.edu>
2017-10-22 23:01:39 -0500

    added progress bar

--- ChildGrowth/ChildGrowth/ChildGrowth/Pages/Vaccinations/Vaccinations.xaml.cs
+++ ChildGrowth/ChildGrowth/ChildGrowth/Pages/Vaccinations/Vaccinations.xaml.cs
@@ -13,7 +13,7 @@ namespace ChildGrowth.Pages.Vaccinations
     public partial class Vaccinations : ContentPage
     {
 
-        public List<VaccinationTable> Vaccines = new List<VaccinationTable>();
+        public static List<VaccinationTable> Vaccines = new List<VaccinationTable>();
 
         ListView vaccinationList = new ListView
         {
@@ -22,24 +22,54 @@ namespace ChildGrowth.Pages.Vaccinations
 
         public Vaccinations()
         {
+            var layout = new StackLayout();
+
             BackgroundColor = Color.FromRgb(94, 196, 225);
 
             VaccinationRepository();
+            
+
+            
+
 
             vaccinationList.ItemsSource = Vaccines;
-            //vaccinationList.ItemTemplate = new DataTemplate(typeof(EveryVaccinationCell));
+            vaccinationList.ItemTemplate = new DataTemplate(typeof(VaccinationCell));
             vaccinationList.BackgroundColor = Color.Transparent;
             vaccinationList.SeparatorColor = Color.White;
-            //vaccinationList.ItemSelected += (sender, e) => {
-              //  ((ListView)sender).SelectedItem = null;
-            //};
+            vaccinationList.ItemSelected += (sender, e) => {
+                ((ListView)sender).SelectedItem = null;
+            };
+
+            vaccinationList.ItemTapped += (Sender, Event) => {
+
+               
+            };
+
+            Content = new StackLayout
+            {
+                VerticalOptions = LayoutOptions.FillAndExpand,
+                Padding = new Thickness(0, 10, 0, 10),
+                Children = {
+                  new ProgressBar
+            {
+                Progress = .2,
+            },
+            vaccinationList
+                }
+            };
+
+            
+        }
+
+        protected override void OnAppearing()
+        {
+           
         }
 
         public void VaccinationRepository()
         {
             Vaccines.Add(new VaccinationTable() { VaccinationID = 1, Name = "Shot1", Info = "Enlarge Penis", Time = new DateTime(2017, 12, 1) });
             Vaccines.Add(new VaccinationTable() { VaccinationID = 2, Name = "Shot2", Info = "Enlarge Brain", Time = new DateTime(2017, 12, 2) });
-
         }
     }
 }
