7f8303e8a9043f1db596e209d86255734b8ae0a3
kevinjyee <kevinyee@utexas.edu>
2017-10-22 22:34:42 -0500

    vaccination page

--- ChildGrowth/ChildGrowth/ChildGrowth/Pages/Vaccinations/Vaccinations.xaml.cs
+++ ChildGrowth/ChildGrowth/ChildGrowth/Pages/Vaccinations/Vaccinations.xaml.cs
@@ -12,9 +12,34 @@ namespace ChildGrowth.Pages.Vaccinations
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Vaccinations : ContentPage
     {
+
+        public List<VaccinationTable> Vaccines = new List<VaccinationTable>();
+
+        ListView vaccinationList = new ListView
+        {
+            RowHeight = 40
+        };
+
         public Vaccinations()
         {
-            InitializeComponent();
+            BackgroundColor = Color.FromRgb(94, 196, 225);
+
+            VaccinationRepository();
+
+            vaccinationList.ItemsSource = Vaccines;
+            //vaccinationList.ItemTemplate = new DataTemplate(typeof(EveryVaccinationCell));
+            vaccinationList.BackgroundColor = Color.Transparent;
+            vaccinationList.SeparatorColor = Color.White;
+            //vaccinationList.ItemSelected += (sender, e) => {
+              //  ((ListView)sender).SelectedItem = null;
+            //};
+        }
+
+        public void VaccinationRepository()
+        {
+            Vaccines.Add(new VaccinationTable() { VaccinationID = 1, Name = "Shot1", Info = "Enlarge Penis", Time = new DateTime(2017, 12, 1) });
+            Vaccines.Add(new VaccinationTable() { VaccinationID = 2, Name = "Shot2", Info = "Enlarge Brain", Time = new DateTime(2017, 12, 2) });
+
         }
     }
 }
