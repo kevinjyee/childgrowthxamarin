5b45abaa1af0a08c1f5c366d15aaaf781549a069
kevinjyee <kevinyee@utexas.edu>
2017-10-27 10:44:45 -0500

    modified education popup

--- ChildGrowth/ChildGrowth/ChildGrowth/Pages/Education/Education.xaml.cs
+++ ChildGrowth/ChildGrowth/ChildGrowth/Pages/Education/Education.xaml.cs
@@ -16,5 +16,61 @@ namespace ChildGrowth.Pages.Education
         {
             InitializeComponent();
         }
+
+        async void BehaviorButtonClick(object sender, EventArgs args)
+        {
+           // await Navigation.PushModalAsync(new EducationInfoDisplay(Models.Education.EducationCategory.BEHAVIOR.ToString()));
+        }
+
+        async void LearningButtonClick(object sender, EventArgs e)
+        {
+            //await Navigation.PushModalAsync(new EducationInfoDisplay(Models.Education.EducationCategory.LEARNING.ToString()));
+        }
+
+        async void SafetyButtonClick(object sender, EventArgs e)
+        {
+            //await Navigation.PushModalAsync(new EducationInfoDisplay(Models.Education.EducationCategory.SAFETY.ToString()));
+        }
+
+        async void PlayButtonClick(object sender, EventArgs e)
+        {
+           // await Navigation.PushModalAsync(new EducationInfoDisplay(Models.Education.EducationCategory.PLAY.ToString()));
+        }
+
+        async void ToiletTrainingButtonClick(object sender, EventArgs e)
+        {
+           // await Navigation.PushModalAsync(new EducationInfoDisplay(Models.Education.EducationCategory.TOILET_TRAINING.ToString()));
+        }
+
+        async void DiseasesButtonClick(object sender, EventArgs e)
+        {
+           // await Navigation.PushModalAsync(new EducationInfoDisplay(Models.Education.EducationCategory.DISEASES.ToString()));
+        }
+
+        async void DoctorVisitsButtonClick(object sender, EventArgs e)
+        {
+           // await Navigation.PushModalAsync(new EducationInfoDisplay(Models.Education.EducationCategory.DOCTOR_VISITS.ToString()));
+        }
+        // with the yearXX clicks, need to reroute to button page
+        async void Year01ButtonClick(object sender, EventArgs e)
+        {
+           // var year01ButtonNavPage = new Year01ButtonNav();
+
+           // await Navigation.PushModalAsync(year01ButtonNavPage);
+        }
+
+        async void Year12ButtonClick(object sender, EventArgs e)
+        {
+           // var year12ButtonNavPage = new Year12ButtonNav();
+
+            //await Navigation.PushModalAsync(year12ButtonNavPage);
+        }
+
+        async void Year23ButtonClick(object sender, EventArgs e)
+        {
+            //var year23ButtonNavPage = new Year23ButtonNav();
+
+           // await Navigation.PushModalAsync(year23ButtonNavPage);
+        }
     }
 }
---
+++ ChildGrowth/ChildGrowth/ChildGrowth/Pages/Education/EducationInfo.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace ChildGrowth.Pages.Education
+{
+    public class EducationInfo : ContentPage
+    {
+        //    public static List<VaccinationTable> Vaccines = new List<VaccinationTable>();
+
+        //    ListView vaccinationList = new ListView
+        //    {
+        //        RowHeight = 40
+        //    };
+
+        //    static double percentprog = 0.2;
+
+        //    ProgressBar vacProg = new ProgressBar
+        //    {
+        //        Progress = percentprog,
+        //    };
+
+
+        //    public Vaccinations()
+        //    {
+        //        var layout = new StackLayout();
+
+        //        BackgroundColor = Color.FromRgb(94, 196, 225);
+
+        //        VaccinationRepository();
+
+
+
+
+
+        //        vaccinationList.ItemsSource = Vaccines;
+        //        vaccinationList.ItemTemplate = new DataTemplate(typeof(VaccinationCell));
+        //        vaccinationList.BackgroundColor = Color.Transparent;
+        //        vaccinationList.SeparatorColor = Color.White;
+        //        vaccinationList.ItemSelected += (sender, e) => {
+        //            ((ListView)sender).SelectedItem = null;
+        //        };
+
+        //        vaccinationList.ItemSelected += (sender, e) => {
+
+        //            ((ListView)sender).SelectedItem = null;
+
+        //        };
+
+        //        vaccinationList.ItemTapped += (Sender, Event) =>
+        //        {
+
+
+
+
+        //            var V = (VaccinationTable)Event.Item;
+
+        //            Navigation.PushAsync(new VaccinationInfoView(V));
+
+
+
+        //        };
+
+        //        Content = new StackLayout
+        //        {
+        //            VerticalOptions = LayoutOptions.FillAndExpand,
+        //            Padding = new Thickness(0, 10, 0, 10),
+        //            Children = {
+        //              vacProg,
+        //              vaccinationList
+        //            }
+        //        };
+
+
+        //    }
+        //    async void updateProgBar()
+        //    {
+        //        await vacProg.ProgressTo(percentprog, 250, Easing.Linear);
+        //    }
+
+
+        //    protected override void OnAppearing()
+        //    {
+
+        //    }
+
+        //    public void VaccinationRepository()
+        //    {
+        //        Vaccines.Add(new VaccinationTable() { VaccinationID = 1, Name = "Shot1", Info = "Enlarge Penis", Time = 1 });
+        //        Vaccines.Add(new VaccinationTable() { VaccinationID = 2, Name = "Shot2", Info = "Enlarge Big Penis", Time = 2 });
+        //        Vaccines.Add(new VaccinationTable() { VaccinationID = 3, Name = "Shot3", Info = "Enlarge Bigger Penis", Time = 2 });
+        //        Vaccines.Add(new VaccinationTable() { VaccinationID = 4, Name = "Shot4", Info = "Enlarge Bigger Bigger Penis", Time = 2 });
+        //        Vaccines.Add(new VaccinationTable() { VaccinationID = 3, Name = "Shot5", Info = "Enlarge Bigger Penis", Time = 2 });
+        //    }
+        //}
+    }
+}
